e3a838b42388be33d2c8715d2e214d5bbf1f41de
Dnyanesh Patil <dnyaneshpatil77@gmail.com>
2023-02-03 17:30:43 +0530

    project initialize

---
+++ sidebar.php
@@ -0,0 +1,209 @@
+<aside class="main-sidebar sidebar-dark-primary elevation-4">
+  <!-- Brand Logo -->
+  <a href="index.php" class="brand-link">
+    <img src="assets/img/admin.png" alt="Admin" class="brand-image img-circle elevation-3" style="opacity: .8">
+    <span class="brand-text font-weight-light">Admin</span>
+  </a>
+
+  <!-- Sidebar -->
+  <div class="sidebar">
+    <!-- Sidebar user panel (optional) -->
+    <!-- <div class="user-panel mt-3 pb-3 mb-3 d-flex">
+        <div class="image">
+          <img src="dist/img/user2-160x160.jpg" class="img-circle elevation-2" alt="User Image">
+        </div>
+        <div class="info">
+          <a href="#" class="d-block">Alexander Pierce</a>
+        </div>
+      </div> -->
+
+    <!-- SidebarSearch Form -->
+    <div class="form-inline pt-2">
+      <div class="input-group" data-widget="sidebar-search">
+        <input class="form-control form-control-sidebar" type="search" placeholder="Search" aria-label="Search">
+        <div class="input-group-append">
+          <button class="btn btn-sidebar">
+            <i class="fas fa-search fa-fw"></i>
+          </button>
+        </div>
+      </div>
+    </div>
+
+    <!-- Sidebar Menu -->
+    <nav class="mt-2">
+      <ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu" data-accordion="false">
+        <!-- Add icons to the links using the .nav-icon class
+               with font-awesome or any other icon font library -->
+        <li class="nav-item menu-open">
+          <a href="#" class="nav-link">
+            <i class="nav-icon fas fa-circle"></i>
+            <p>
+              Dashboard
+              <i class="right fas fa-angle-left"></i>
+            </p>
+          </a>
+          <ul class="nav nav-treeview">
+            <li class="nav-item">
+              <a href="./index.php" class="nav-link">
+                <i class="far fa-circle nav-icon text-danger"></i>
+                <p>Page 1</p>
+              </a>
+            </li>
+            <li class="nav-item">
+              <a href="./index2.php" class="nav-link">
+                <i class="far fa-circle nav-icon text-info"></i>
+                <p>Page 2</p>
+              </a>
+            </li>
+            <li class="nav-item">
+              <a href="./index3.php" class="nav-link">
+                <i class="far fa-circle nav-icon text-warning"></i>
+                <p>Page 3</p>
+              </a>
+            </li>
+          </ul>
+        </li>
+        <li class="nav-item">
+          <a href="#" class="nav-link">
+            <i class="nav-icon fas fa-circle"></i>
+            <p>
+              Students
+              <i class="right fas fa-angle-left"></i>
+            </p>
+          </a>
+          <ul class="nav nav-treeview">
+            <li class="nav-item">
+              <a href="./register_students.php" class="nav-link">
+                <i class="far fa-circle nav-icon text-danger"></i>
+                <p>Register Students</p>
+              </a>
+            </li>
+            <li class="nav-item">
+              <a href="./students_list.php" class="nav-link">
+                <i class="far fa-circle nav-icon text-warning"></i>
+                <p>Students list</p>
+              </a>
+            </li>
+          </ul>
+        </li>
+        <li class="nav-item">
+          <a href="#" class="nav-link">
+            <i class="nav-icon fas fa-circle"></i>
+            <p>
+              User
+              <i class="right fas fa-angle-left"></i>
+            </p>
+          </a>
+          <ul class="nav nav-treeview">
+            <li class="nav-item">
+              <a href="./register_user.php" class="nav-link">
+                <i class="far fa-circle nav-icon text-warning"></i>
+                <p>Register User</p>
+              </a>
+            </li>
+            <li class="nav-item">
+              <a href="user_list.php" class="nav-link">
+                <i class="far fa-circle nav-icon text-danger"></i>
+                <p>User list</p>
+              </a>
+            </li>
+          </ul>
+        </li>
+        <li class="nav-item">
+          <a href="#" class="nav-link">
+            <i class="nav-icon fas fa-circle"></i>
+            <p>
+              Prakalpa
+              <i class="right fas fa-angle-left"></i>
+            </p>
+          </a>
+          <ul class="nav nav-treeview">
+            <li class="nav-item">
+              <a href="./register_praklpa.php" class="nav-link">
+                <i class="far fa-circle nav-icon text-warning"></i>
+                <p>Register Prakalpa</p>
+              </a>
+            </li>
+            <li class="nav-item">
+              <a href="prakalpa_list.php" class="nav-link">
+                <i class="far fa-circle nav-icon text-danger"></i>
+                <p>Prakalpa list</p>
+              </a>
+            </li>
+          </ul>
+        </li>
+        <li class="nav-item">
+          <a href="#" class="nav-link">
+            <i class="nav-icon fas fa-circle"></i>
+            <p>
+              Bit
+              <i class="right fas fa-angle-left"></i>
+            </p>
+          </a>
+          <ul class="nav nav-treeview">
+            <li class="nav-item">
+              <a href="./register_bit.php" class="nav-link">
+                <i class="far fa-circle nav-icon text-warning"></i>
+                <p>Register Bit</p>
+              </a>
+            </li>
+            <li class="nav-item">
+              <a href="bit_list.php" class="nav-link">
+                <i class="far fa-circle nav-icon text-danger"></i>
+                <p>Bit list</p>
+              </a>
+            </li>
+          </ul>
+        </li>
+        <li class="nav-item">
+          <a href="#" class="nav-link">
+            <i class="nav-icon fas fa-circle"></i>
+            <p>
+              Aganwadi
+              <i class="right fas fa-angle-left"></i>
+            </p>
+          </a>
+          <ul class="nav nav-treeview">
+            <li class="nav-item">
+              <a href="./register_aganwadi.php" class="nav-link">
+                <i class="far fa-circle nav-icon text-warning"></i>
+                <p>Register Aganwadi</p>
+              </a>
+            </li>
+            <li class="nav-item">
+              <a href="./aganwadi_list.php" class="nav-link">
+                <i class="far fa-circle nav-icon text-danger"></i>
+                <p>Aganwadi list</p>
+              </a>
+            </li>
+          </ul>
+        </li>
+        <li class="nav-item">
+          <a href="#" class="nav-link">
+            <i class="nav-icon fas fa-circle"></i>
+            <p>
+              Aganwadi User
+              <i class="right fas fa-angle-left"></i>
+            </p>
+          </a>
+          <ul class="nav nav-treeview">
+            <li class="nav-item">
+              <a href="./aganwadi_user.php" class="nav-link">
+                <i class="far fa-circle nav-icon text-warning"></i>
+                <p>Register Aganwadi User</p>
+              </a>
+            </li>
+            <li class="nav-item">
+              <a href="./aganwadi_user_list.php" class="nav-link">
+                <i class="far fa-circle nav-icon text-danger"></i>
+                <p>Aganwadi User list</p>
+              </a>
+            </li>
+          </ul>
+        </li>
+      </ul>
+    </nav>
+    <!-- /.sidebar-menu -->
+  </div>
+  <!-- /.sidebar -->
+</aside>
