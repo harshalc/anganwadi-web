c77b4591e057b480856972e4c943252fb894441c
pranalisonje <pranali@kitintellect.com>
2023-02-10 12:57:52 +0530

    admin

--- include-sidebar.php
+++ include-sidebar.php
@@ -66,23 +66,23 @@
     <span class="brand-text font-weight-light">Admin</span>
   </a>
  
-  <a href="#" class="brand-link">
+  <!-- <a href="#" class="brand-link">
     <img src="assets/img/admin.png" alt="Admin" class="brand-image img-circle elevation-3" style="opacity: .8">
     <span class="brand-text font-weight-light">Admin</span><br>
 
     <span class="brand-text font-weight-light" id="user_login_name">pranali</span>
-  </a>
+  </a> -->
   <!-- Sidebar -->
   <div class="sidebar">
     <!-- Sidebar user panel (optional) -->
-    <!-- <div class="user-panel mt-3 pb-3 mb-3 d-flex">
+    <div class="user-panel mt-3 pb-3 mb-3 d-flex">
         <div class="image">
           <img src="dist/img/user2-160x160.jpg" class="img-circle elevation-2" alt="User Image">
         </div>
         <div class="info">
           <a href="#" class="d-block">Alexander Pierce</a>
         </div>
-      </div> -->
+      </div>
 
     <!-- SidebarSearch Form -->
     <div class="form-inline pt-2">
